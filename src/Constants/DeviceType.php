f24dd7d51a6b8a1b13e2e34847807585fad840bb
Avik Aghajanyan <avik.aghajanyan@gmail.com>
2018-02-14 18:25:34 +0400

    Added Jenssegers date and agent packages

---
+++ src/Constants/DeviceType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ptuchik\CoreUtilities\Constants;
+
+use Ptuchik\CoreUtilities\AbstractClasses\AbstractTypes;
+
+/**
+ * Class DeviceType
+ * @package Ptuchik\CoreUtilities\Constants
+ */
+class DeviceType extends AbstractTypes
+{
+    const ALL = 1;
+    const DESKTOP = 2;
+    const MOBILE = 3;
+}
